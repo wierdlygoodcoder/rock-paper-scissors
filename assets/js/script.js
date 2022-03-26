db6814621b0bf41b8ec5dc6e9c228bf9e4c5f407
wierdlygoodcoder <ashleyrolfe954@msn.com>
2022-03-26 03:26:28 +0000

    styled game so that it matchs intro section

---
+++ assets/js/script.js
@@ -0,0 +1,25 @@
+let [computerScore, playerScore]=[0,0];
+let resultScore = document.getElementById("score");
+let choices = {
+    'rock': {
+        'rock': 'draw',
+        'paper':'win',
+        'scissor':'lose',
+    },
+        'paper': {
+        'paper': 'draw',
+        'scissor':'win',
+        'rock':'lose',
+    } ,
+    'scissor': {
+        'scissor': 'draw',
+        'rock':'win',
+        'paper':'lose',
+    }
+
+}
+let intro = document.getElementById('intro')
+document.getElementById('play').addEventListener('click',function(){
+    intro.classList.toggle('fade');
+})
+
