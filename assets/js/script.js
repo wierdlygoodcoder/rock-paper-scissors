4febcf25ce00d9a79d63e106d47f7f7a4af1a01a
wierdlygoodcoder <ashleyrolfe954@msn.com>
2022-03-30 10:25:07 +0000

    finished the game logic and started to change images to what the user clicks

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,67 +3,78 @@ let computerScore = 0;
 let userscore_div = document.getElementById("playerscore");
 let computerScore_div = document.getElementById("computerscore");
 let scoreBoard_div = document.querySelector(".scoreboard");
-let result_div = document.querySelector(".result");
+let result_p = document.querySelector(".results > p");
 let paper_div = document.getElementById("paper");
 let rock_div = document.getElementById("rock");
 let scissors_div = document.getElementById("scissors");
 
 function computerChoice(){
     let choices =['paper','rock','scissors'];
-    let randomnum = Math.floor(Math.random()*3);
-    return choices [randomnum];
+    let randomnum = Math.floor(Math.random()*choices.length);
+    document.getElementById()
+    return choices[randomnum];
 }
 
-function win(){
+function convertToCapitals(word) {
+    if (word === "rock") return "Rock";
+    if (word === "paper") return "Paper";
+    return "Scissors";
+}
+
+
+function win(userChoice,compChosse) {
     playerScore++;
     userscore_div .innerHTML = playerScore;
-    computerScore_div .innerHTML = computerScore
+    computerScore_div .innerHTML = computerScore;
+    result_p.innerHTML = `${convertToCapitals(userChoice)} Smashes ${convertToCapitals(compChosse)}. WINNER!!!`;
 }
 
-function draw(){
-    console.log("Draw")
+function draw(userChoice,compChosse){
+    result_p.innerHTML = `${convertToCapitals(userChoice)} equals ${convertToCapitals(compChosse)}. YOU DRAW`
 }
 
-function lose(){
-    console.log("lose")
+function lose(userChoice,compChosse){
+    computerScore++;
+    computerScore_div.innerHTML = computerScore;
+    result_p.innerHTML =`${convertToCapitals(userChoice)} obliterates ${convertToCapitals(compChosse)}. LOSER :(`
 }
 
 
 function game(userChoice){
-    let compChosse =computerChoice();
+    let compChosse = computerChoice();
     switch (userChoice + compChosse){
         case "rockscissors":
         case "paperrock":
         case "scissorspaper":
-            win()
+            win(userChoice, compChosse);
         break;
         case "rockpaper":
         case "paperscissors":
         case "scissorsrock":
-            lose()
+            lose(userChoice, compChosse);
          break;
         case "scissorsscissors":
         case "paperpaper":
         case "rockrock":
-            draw()
+            draw(userChoice, compChosse);
     }
 }
 
 function main(){
 rock_div.addEventListener('click', function(){
-    game("rock")
+    game("rock");
   
-})
+});
 
 paper_div.addEventListener('click', function(){
-    game("paper")
+    game("paper");
    
-})
+});
 
 scissors_div.addEventListener('click', function(){
-    game("scissors")
+    game("scissors");
     
-})
+});
 }
 
 main();
@@ -78,3 +89,14 @@ let match = document.getElementById('match');
     document.getElementById('play').addEventListener('click',function(){
     match.classList.toggle('fadeIn');
 });
+
+function changeImage() {
+    let image = document.getElementById('player-hand');
+    if(image.src.match("paper")){
+        image.src = "rock.png";
+    }else{
+        image.src = "assets/images/paper.png"
+    
+    }
+    
+}
