6112b3837a82d8ded7848dbf08ca872328fa7e30
wierdlygoodcoder <ashleyrolfe954@msn.com>
2022-04-05 00:29:12 +0000

    cleaned up the game so it looks better

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,17 +12,17 @@ let playerHand = document.getElementById('player-hand');
 let computerHand = document.getElementById('computer-hand');
 let gameOver = 5;
 let modalBtn = document.querySelector(".rules-btn");
-let modalBg =document.querySelector(".rules-bg");
+let modalBg = document.querySelector(".rules-bg");
 let modalClose = document.querySelector(".close");
 
-function computerChoice(){
-    let choices =['paper','rock','scissors'];
-    let randomNum = Math.floor(Math.random()*choices.length);
+function computerChoice() {
+    let choices = ['paper', 'rock', 'scissors', 'lizard', 'spock'];
+    let randomNum = Math.floor(Math.random() * choices.length);
     return choices[randomNum];
 }
 
 function changeImage(imgName, target) {
-    target.src = `assets/images/${imgName}.png`;  
+    target.src = `assets/images/${imgName}.png`;
 }
 
 function convertToCapitals(word) {
@@ -34,27 +34,27 @@ function convertToCapitals(word) {
 }
 
 
-function win(userChoice,compChosse) {
+function win(userChoice, compChosse) {
     playerScore++;
-    userscoreDiv .innerHTML = playerScore;
-    computerScoreDiv .innerHTML = computerScore;
+    userscoreDiv.innerHTML = playerScore;
+    computerScoreDiv.innerHTML = computerScore;
     resultP.innerHTML = `${convertToCapitals(userChoice)} Smashes ${convertToCapitals(compChosse)}. WINNER!!!`;
     if (playerScore === gameOver) {
         gameOverFunction('player');
-        }
+    }
 }
 
-function draw(userChoice,compChosse){
+function draw(userChoice, compChosse) {
     resultP.innerHTML = `${convertToCapitals(userChoice)} equals ${convertToCapitals(compChosse)}. YOU DRAW`;
 }
 
-function lose(userChoice,compChosse){
+function lose(userChoice, compChosse) {
     computerScore++;
     computerScoreDiv.innerHTML = computerScore;
-    resultP.innerHTML =`${convertToCapitals(userChoice)} obliterates ${convertToCapitals(compChosse)}. LOSER :(`;
+    resultP.innerHTML = `${convertToCapitals(userChoice)} obliterates ${convertToCapitals(compChosse)}. LOSER :(`;
     if (computerScore === gameOver) {
         gameOverFunction('computer');
-        }
+    }
 }
 
 function gameOverFunction(winner) {
@@ -62,14 +62,14 @@ function gameOverFunction(winner) {
     setTimeout(() => {
         location.reload();
     }, 2000);
-    
+
 }
 
-function game(userChoice){
+function game(userChoice) {
     let compChosse = computerChoice();
     changeImage(userChoice, playerHand);
     changeImage(compChosse, computerHand);
-    switch (userChoice + compChosse){
+    switch (userChoice + compChosse) {
         case "rockscissors":
         case "rocklizard":
         case "paperrock":
@@ -81,7 +81,7 @@ function game(userChoice){
         case "spockscissor":
         case "spockrock":
             win(userChoice, compChosse);
-        break;
+            break;
         case "rockpaper":
         case "rockspock":
         case "paperscissors":
@@ -93,7 +93,7 @@ function game(userChoice){
         case "spockpaper":
         case "spocklizard":
             lose(userChoice, compChosse);
-         break;
+            break;
         case "scissorsscissors":
         case "paperpaper":
         case "rockrock":
@@ -103,24 +103,24 @@ function game(userChoice){
     }
 }
 
-function main(){
-    rockSpan.addEventListener('click', function(){
+function main() {
+    rockSpan.addEventListener('click', function () {
         game("rock");
     });
 
-    paperSpan.addEventListener('click', function(){
+    paperSpan.addEventListener('click', function () {
         game("paper");
     });
 
-    scissorsSpan.addEventListener('click', function(){
+    scissorsSpan.addEventListener('click', function () {
         game("scissors");
     });
 
-    spockSpan.addEventListener('click', function(){
+    spockSpan.addEventListener('click', function () {
         game("spock");
     });
 
-    lizardSpan.addEventListener('click', function(){
+    lizardSpan.addEventListener('click', function () {
         game("lizard");
     });
 }
@@ -130,20 +130,19 @@ main();
 
 
 let intro = document.getElementById('intro');
-document.getElementById('play').addEventListener('click',function(){
+document.getElementById('play').addEventListener('click', function () {
     intro.classList.toggle('fade');
 });
 
 let match = document.getElementById('match');
-document.getElementById('play').addEventListener('click',function(){
+document.getElementById('play').addEventListener('click', function () {
     match.classList.toggle('fadeIn');
 });
 
-modalBtn.addEventListener('click', function(){
+modalBtn.addEventListener('click', function () {
     modalBg.classList.add('bg-active');
 })
 
-modalClose.addEventListener('click', function(){
+modalClose.addEventListener('click', function () {
     modalBg.classList.remove('bg-active')
 })
-
